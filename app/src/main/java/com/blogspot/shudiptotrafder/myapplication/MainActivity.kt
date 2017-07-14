668de286587a38f85f6dfe75af9b38e55c1ca22f
Iamsdt <shudiptotrafder@gmail.com>
2017-07-15 00:36:22 +0600

    Final

--- app/src/main/java/com/blogspot/shudiptotrafder/myapplication/MainActivity.kt
+++ app/src/main/java/com/blogspot/shudiptotrafder/myapplication/MainActivity.kt
@@ -1,48 +1,22 @@
 package com.blogspot.shudiptotrafder.myapplication
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
-    private var pointP1 = 0
-    private var pointP2 = 0
-
-    //saved instance key
-    private var pointP1Key = "p1"
-    private var pointP2Key = "p2"
-
-
-    val spName = "sp"
-    var pref: SharedPreferences? = null
-
-    var increment = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        pref = getSharedPreferences(spName, Context.MODE_PRIVATE)
-
-        pointP1 = pref!!.getInt(pointP1Key, 0)
-        pointP2 = pref!!.getInt(pointP2Key, 0)
-
-        tv_player1.text = "Player 1:$pointP1"
-        tv_player2.text = "Player 2:$pointP2"
-
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Want to Play again", Snackbar.LENGTH_LONG)
@@ -84,7 +58,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun playGame(cellID: Int, buSelected: Button) {
 
-        if (!buSelected.isEnabled) {
+        if (!buSelected.isEnabled){
             return
         }
 
@@ -110,177 +84,97 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkWinner() {
 
-        var winner = -1
+        var winer = -1
 
-        //row 1
-        //player 1
-        if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
-            winner = 1
-
-        } else if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
-            //player2
-            winner = 2
+        // row 1
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+            winer=1
+        }
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            winer=2
         }
 
-        //row 2
-        //player 1
-        else if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
-            winner = 1
+
+        // row 2
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            winer=1
+        }
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            winer=2
         }
 
-        //player 2
-        else if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
-            winner = 2
+
+
+
+        // row 3
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            winer=1
+        }
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            winer=2
         }
 
-        //row 3
-        //player 1
-        else if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
-            winner = 1
+
+
+        // col 1
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            winer=1
+        }
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            winer=2
         }
 
-        //player 2
-        else if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
-            winner = 2
+
+
+        // col 2
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            winer=1
+        }
+        if(player2.contains(2) && player2.contains(5) && player2.contains(8)){
+            winer=2
         }
 
-        //col 1
-        //player 1
-        else if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
-            winner = 1
-        }
 
-        //player 2
-        else if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
-            winner = 2
+        // col 3
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            winer=1
         }
-
-        //col 2
-        //player 1
-        else if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
-            winner = 1
-        }
-
-        //player 2
-        else if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
-            winner = 2
-        }
-
-        //col 3
-        //player 1
-        else if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
-            winner = 1
-        }
-
-        //player 2
-        else if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
-            winner = 2
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            winer=2
         }
 
         //cross connection
-        else if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
-            winner = 1
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+            winer = 1
         }
 
         //player 2
-        else if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
-            winner = 2
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+            winer = 2
         }
 
         //player 1
-        else if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
-            winner = 1
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+            winer = 1
         }
 
         //player 2
-        else if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
-            winner = 2
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+            winer = 2
         }
 
+        if (winer != -1) {
 
-
-        if (winner != -1) {
-
-            if (increment) {
-                return
-            }
-
-            if (winner == 1) {
-                pointP1 += 1
-                tv_player1.text = "Player 1:$pointP1"
-                Snackbar.make(container, "player 1 win", Snackbar.LENGTH_SHORT).show()
-                btnClear()
-            } else if (winner == 2) {
-                pointP2 += 1
-                tv_player2.text = "Player 2:$pointP2"
-                Snackbar.make(container, "player 1 win", Snackbar.LENGTH_SHORT).show()
-                btnClear()
+            if (winer==1){
+                Toast.makeText(this," Player 1  win the game", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this," Player 2  win the game", Toast.LENGTH_LONG).show()
 
             }
 
-            val editor = pref!!.edit()
-            editor.putInt(pointP1Key, pointP1)
-            editor.putInt(pointP2Key, pointP2)
-            editor.apply()
-
-            increment = true
         }
     }
 
-    private fun btnClear(){
-
-        //set increment to false so that it can be increment again
-        increment = false
-
-        //clear all saved data
-        player1.clear()
-        player2.clear()
-        //set active palyer to 1
-        ActivePlayer = 1
-
-        bu1.isEnabled = true
-        bu1.setBackgroundResource(R.color.btnBcg)
-        bu1.text = ""
-
-        bu2.isEnabled = true
-        bu2.setBackgroundResource(R.color.btnBcg)
-        bu2.text = ""
-
-        bu3.isEnabled = true
-        bu3.setBackgroundResource(R.color.btnBcg)
-        bu3.text = ""
-
-        bu4.isEnabled = true
-        bu4.setBackgroundResource(R.color.btnBcg)
-        bu4.text = ""
-
-        bu5.isEnabled = true
-        bu5.setBackgroundResource(R.color.btnBcg)
-        bu5.text = ""
-
-        bu6.isEnabled = true
-        bu6.setBackgroundResource(R.color.btnBcg)
-        bu6.text = ""
-
-        bu7.isEnabled = true
-        bu7.setBackgroundResource(R.color.btnBcg)
-        bu7.text = ""
-
-        bu8.isEnabled = true
-        bu8.setBackgroundResource(R.color.btnBcg)
-        bu8.text = ""
-
-        bu9.isEnabled = true
-        bu9.setBackgroundResource(R.color.btnBcg)
-        bu9.text = ""
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        val edit = pref!!.edit()
-        edit.clear()
-        edit.apply()
-    }
 
     fun autoPlay() {
 
@@ -294,52 +188,36 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        val random = Random()
-        val randIndex: Int
+            val random = Random()
+            val randIndex: Int
 
-        var cellID = 0
+            var cellID = 0
 
-        try {
-            randIndex = random.nextInt(emptyCell.size - 0) + 0
-            cellID = emptyCell[randIndex]
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("Random", e.message)
-        }
+            try {
+                randIndex = random.nextInt(emptyCell.size - 0) + 0
+                cellID = emptyCell[randIndex]
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Log.e("Random", e.message)
+            }
 
-        val buttonSelected: Button?
+            val buttonSelected: Button?
 
-        when (cellID) {
-            1 -> buttonSelected = bu1
-            2 -> buttonSelected = bu2
-            3 -> buttonSelected = bu3
-            4 -> buttonSelected = bu4
-            5 -> buttonSelected = bu5
-            6 -> buttonSelected = bu6
-            7 -> buttonSelected = bu7
-            8 -> buttonSelected = bu8
-            9 -> buttonSelected = bu9
+            when (cellID) {
+                1 -> buttonSelected = bu1
+                2 -> buttonSelected = bu2
+                3 -> buttonSelected = bu3
+                4 -> buttonSelected = bu4
+                5 -> buttonSelected = bu5
+                6 -> buttonSelected = bu6
+                7 -> buttonSelected = bu7
+                8 -> buttonSelected = bu8
+                9 -> buttonSelected = bu9
 
-            else ->
-                buttonSelected = bu1
-        }
+                else ->
+                    buttonSelected = bu1
+            }
 
-        playGame(cellID, buttonSelected)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+            playGame(cellID, buttonSelected)
     }
 }
